0d301408c1cd1706707f8db1668f8d163eca6636
Tushar-senpai <tk9471653832@gmail.com>
2025-08-07 10:05:18 +0530

    initial commit and setups

---
+++ Backend/src/controllers/auth.contoller.js
@@ -0,0 +1,14 @@
+export const signup = async (req, res) => {
+  (req, res) => {
+  res.send("login route");}
+}
+
+export const login = async (req, res) => {
+  (req, res) => {
+  res.send("login route");}
+}
+
+export const logout= async (req, res) => {
+  (req, res) => {
+  res.send("login route");}
+}
