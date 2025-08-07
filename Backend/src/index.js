be51961875270c29545f256024abc49ee4d28d3a
Tushar-senpai <tk9471653832@gmail.com>
2025-08-08 00:41:46 +0530

    adding database mongo db

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import {connectDB} from './lib/db.js';
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT ;
 app.use ("/api/auth", authRoutes);
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.listen(PORT, () => {
+  console.log('Server is running on port:'+ PORT);
+  connectDB();
 });
