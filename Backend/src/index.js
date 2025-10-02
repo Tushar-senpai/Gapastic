9d3f97e9471a1665d0d862c2ae58078f0f2f30e8
tushar kumar <tk9471653832@gmail.com>
2025-10-03 00:19:23 +0530

    revied backend code with up to message routes set up

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,6 +3,9 @@ import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import messageRoutes from "./routes/message.route.js";
+
+
 
 dotenv.config();
 const app = express();
@@ -13,6 +16,7 @@ app.use(express.json());
 
 
 app.use ("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.use(cookieParser());
 
 
